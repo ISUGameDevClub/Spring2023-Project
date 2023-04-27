179d253c5b833ed087686c19d8643df7ba7d88c4
OrbitalCodeRock <37189245+OrbitalCodeRock@users.noreply.github.com>
2023-04-27 01:49:09 -0500

    Added state text UI, applied prefab changes

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -8,17 +8,25 @@ public class UI : MonoBehaviour
 {
     private CurrencyManager currencyManager;
     public TMP_Text moneyText;
+    public TMP_Text stateText;
 
+    private StateController stateController;
+
+    private void Awake()
+    {
+        stateController = GameManager.instance.GetComponent<StateController>();
+    }
     // Start is called before the first frame update
     void Start()
     {
         currencyManager = FindObjectOfType<CurrencyManager>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         moneyText.text = "Money: $" + currencyManager.GetPlayerCurrency();
+        stateText.text = "Current State - " + stateController.getStateString();
     }
+
 }

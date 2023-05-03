fda64d97dbb3c86aefdf6a222fc1d7c4e092e410
Gizmo770 <71050882+Gizmo770@users.noreply.github.com>
2023-05-03 00:05:19 -0500

    UI/UpgradeManagerUpdate

---
+++ Assets/UniversalUpgradeSys.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniversalUpgradeSys : MonoBehaviour
+{
+    [SerializeField] private TowerUI towerUI;
+    [SerializeField] private List<TowerUpgrade> towerUpgrades = new List<TowerUpgrade>();
+
+    private void Start()
+    {
+        towerUI = FindObjectOfType<TowerUI>();
+
+        // Initialize the tower levels to 0
+        for (int i = 0; i < towerUpgrades.Count; i++)
+        {
+            towerUpgrades[i].level = 0;
+        }
+    }
+
+    public void UpgradeTower(int towerIndex)
+    {
+        if (towerIndex < 0 || towerIndex >= towerUI.towerPrefabs.Length)
+        {
+            Debug.LogError("Invalid tower index!");
+            return;
+        }
+
+        GameObject towerPrefab = towerUI.towerPrefabs[towerIndex];
+
+        // Get the tower health and attack components
+        TowerHealth towerHealth = towerPrefab.GetComponent<TowerHealth>();
+        TowerAttack towerAttack = towerPrefab.GetComponent<TowerAttack>();
+
+        if (towerHealth == null || towerAttack == null)
+        {
+            Debug.LogError("Tower prefab is missing health or attack component!");
+            return;
+        }
+
+        // Get the upgrade values for the current level of the tower
+        TowerUpgrade upgrade = towerUpgrades[towerIndex];
+        int health = upgrade.healthLevels[upgrade.level];
+        int damage = upgrade.damageLevels[upgrade.level];
+        float attackSpeed = upgrade.attackSpeedLevels[upgrade.level];
+        int range = upgrade.rangeLevels[upgrade.level];
+
+        // Upgrade the tower health and attack components
+        towerHealth.setHealth(health);
+        towerAttack.UpgradeAttackDamage(damage);
+        towerAttack.UpgradeAttackSpeed(attackSpeed);
+        towerAttack.UpgradeRange(range);
+
+        // Increase the tower level for the next upgrade
+        upgrade.level = Mathf.Min(upgrade.level + 1, upgrade.healthLevels.Count - 1);
+    }
+}
+
+[System.Serializable]
+public class TowerUpgrade
+{
+    public List<int> healthLevels = new List<int>();
+    public List<int> damageLevels = new List<int>();
+    public List<float> attackSpeedLevels = new List<float>();
+    public List<int> rangeLevels = new List<int>();
+    public int level;
+}
